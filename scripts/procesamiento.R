784542383987fdd8a89e9c6ac3ae5071642b861a
matiaspoullain <58349277+matiaspoullain@users.noreply.github.com>
2021-04-29 13:03:21 -0300

    uso de zip

--- scripts/procesamiento.R
+++ scripts/procesamiento.R
@@ -1,7 +1,7 @@
 #library(data.table)
 #library(dplyr)
-#datos <- fread(unzip("data/Covid19Casos2021 reducido.zip", "Covid19Casos2021 reducido.csv"), encoding = "UTF-8", stringsAsFactors = TRUE)
-datos <- fread("data/Covid19Casos2021 reducido.csv", encoding = "UTF-8", stringsAsFactors = TRUE)
+datos <- fread(unzip("data/Covid19Casos2021 reducido.zip", "Covid19Casos2021 reducido.csv"), encoding = "UTF-8", stringsAsFactors = TRUE)
+#datos <- fread("data/Covid19Casos2021 reducido.csv", encoding = "UTF-8", stringsAsFactors = TRUE)
 
 #Borro columnas que no nos interesan:
 #datos <- datos[, -c("residencia_pais_nombre",
