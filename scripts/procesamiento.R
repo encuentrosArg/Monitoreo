51ee6d0d68d995f89d57dc794d9029b130d0e213
matiaspoullain <58349277+matiaspoullain@users.noreply.github.com>
2021-05-18 18:21:39 -0300

    Preprocesamiento imporatnte, las bases que se suben son chicas y la pagina es mucho mas estable

--- scripts/procesamiento.R
+++ scripts/procesamiento.R
@@ -1,31 +1,12 @@
 #library(data.table)
 #library(dplyr)
-#datos <- fread(unzip("data/Covid19Casos2021 reducido.zip", "Covid19Casos2021 reducido.csv"), encoding = "UTF-8", stringsAsFactors = TRUE)
-datos <- fread("data/Covid19Casos2021 reducido.csv", encoding = "UTF-8", stringsAsFactors = TRUE)
+#datos <- fread("data/Covid19Casos2021 reducido.csv", encoding = "UTF-8", stringsAsFactors = TRUE)
+datos_r_edad <- fread("data/base_grande_r_edad.csv")
+datos <- fread("data/base_grande.csv")
 
-#Borro columnas que no nos interesan:
-#datos <- datos[, -c("residencia_pais_nombre",
-#      "residencia_provincia_nombre",
-#      "residencia_departamento_nombre",
-#      "carga_provincia_id",
-#      "carga_provincia_nombre",
-#      "sepi_apertura",
-#      "origen_financiamiento",
-#      "clasificacion",
-#      "fecha_diagnostico",
-#      "ultima_actualizacion",
-#      "cuidado_intensivo",
-#      "fallecido",
-#      "asistencia_respiratoria_mecanica")]
-
-#Modifico otras variables ----
-
-#Agrego rangos de edad
-datos[, r_edad := fcase(edad <= 17 , "0-17",
-                            edad > 17 & edad <= 39, "18-39",
-                            edad > 39 & edad <= 59, "40-59",
-                            edad > 59, "60 o más",
-                            default = "N/R")]
+#Fechas maximas y minimas
+fecha_minima <- as.Date("2021-01-01")
+fecha_maxima <- max(datos$fecha_min)
 
 #Codificacion provincia-id y depto id -----
 cod_prov_depto <- fread("data/codigo_prov_depto.csv", encoding = "UTF-8", stringsAsFactors = TRUE)
